22563885196e6610159eb12e9c21daa0134ce07b
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-17 15:42:02 +0200

    add repos list and repo details

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,17 @@
+import Repo from "@/app/components/Repo";
+import Link from "next/link";
+import React from "react";
+
+const RepoPage = ({ params }) => {
+  console.log(params);
+  return (
+    <div className="card">
+      <Link className="btn btn-back" href="/code/repos">
+        Back to Repositories
+      </Link>
+      <Repo name={params.name} />
+    </div>
+  );
+};
+
+export default RepoPage;
---
+++ app/components/Repo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function fetchRepo(name) {
+  const repo = await fetch(`https://api.github.com/repos/saadmosallam/${name}`);
+  return await repo.json();
+}
+
+const Repo = async ({ name }) => {
+  const repo = await fetchRepo(name);
+  return (
+    <>
+      <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className="card-stats">
+        <dev className="card-stat">
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </dev>
+        <dev className="card-stat">
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </dev>
+        <dev className="card-stat">
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </dev>
+      </div>
+    </>
+  );
+};
+
+export default Repo;
