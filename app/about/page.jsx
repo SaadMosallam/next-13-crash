69a401cba5f6f5f23d22e57141e0dd433a4d3cb2
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-17 12:15:07 +0200

    add routes for pages

---
+++ app/about/page.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About app directory</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+        impedit, veniam dicta aliquid inventore nesciunt maiores quaerat
+        tenetur, libero incidunt natus eos perspiciatis esse velit architecto
+        expedita quasi qui non. A suscipit facere labore doloremque at dolor
+        asperiores, odio, corrupti quasi necessitatibus ipsa officiis!
+        Exercitationem eveniet laudantium quas quis. Deleniti quae ex nobis?
+        Doloribus nobis aliquam aut quasi fugit itaque alias quis praesentium,
+        ex reprehenderit maiores inventore sint placeat nesciunt quisquam
+        numquam vel magnam corrupti, aspernatur pariatur laudantium suscipit. Ab
+        atque, doloribus, animi reprehenderit dignissimos blanditiis aut
+        perferendis sequi omnis voluptatem repudiandae, voluptates
+        exercitationem! Vitae unde qui nisi ipsum exercitationem accusantium
+        ipsam cum aut ipsa tenetur, delectus vero laboriosam repellat architecto
+        debitis eius explicabo quos saepe autem ad facere in! Molestiae aperiam
+        possimus expedita fuga illo qui fugit deserunt nemo, voluptatum
+        similique minima sed. Iure placeat tempore ullam vel repellat aut
+        laborum consequatur odit. Quo ducimus iure, illo porro voluptatum
+        aliquid, eaque magni ea dolorem corporis exercitationem tempore labore
+        totam consequuntur sapiente rem error rerum deserunt quaerat odio
+        nostrum iste expedita illum? Aliquid unde eius at laborum mollitia nihil
+        sit veniam saepe alias. Mollitia rem dolore laudantium veniam reiciendis
+        modi culpa id eum exercitationem sint? Adipisci voluptate natus vitae
+        velit explicabo cum, aperiam maxime enim vero quia necessitatibus quidem
+        nobis pariatur, sit eos! Cum enim ipsa, hic praesentium velit fugit
+        distinctio nihil similique. Incidunt accusamus quam reprehenderit. Minus
+        aut distinctio voluptates nobis ducimus explicabo magni. Eligendi
+        officiis, aliquam provident quaerat reiciendis, voluptatum magni fuga
+        asperiores, totam suscipit nemo nihil et error placeat sequi at odio
+        alias mollitia nam ipsam maxime! Dignissimos earum inventore quis,
+        deserunt maiores, neque impedit similique alias odit mollitia modi
+        excepturi architecto itaque iusto, possimus adipisci nobis. Praesentium
+        sit nemo molestias, fugit vel, velit quas aliquam suscipit unde iusto
+        dolorum inventore voluptate facilis? Ex voluptate aliquid expedita
+        eligendi reiciendis facilis quae dignissimos tenetur. Nostrum illo vero
+        earum nemo dolorem quibusdam, inventore cupiditate amet officia.
+        Quibusdam, magnam consequatur voluptates necessitatibus nobis excepturi
+        hic dolorem ut? Illo eius odit qui cupiditate accusantium soluta
+        perspiciatis, reiciendis corrupti iusto error ad impedit repellat
+        aspernatur enim, excepturi dolorem dolores rem debitis explicabo
+        possimus aperiam deserunt voluptates. Nisi aperiam, labore maxime
+        aliquam ratione quas! Quas sapiente molestias atque ipsum at laboriosam
+        quae asperiores libero nostrum consectetur, eos voluptatibus suscipit
+        eius possimus blanditiis reiciendis. Hic, laboriosam soluta eos ducimus
+        distinctio placeat minima repellendus, vel sed commodi sapiente
+        molestiae, obcaecati quae magni nisi! Excepturi architecto minus ex
+        voluptate similique animi quas quia hic magnam id? Suscipit saepe
+        exercitationem pariatur incidunt fugit ad tempora dolorem qui dicta eum,
+        natus vitae consectetur iure iusto sunt itaque at error totam assumenda
+        ratione esse aliquam tenetur harum quam. Totam, fugit eos voluptatibus
+        sunt officia aspernatur minima nobis aperiam, quaerat odio illum
+        reprehenderit dicta beatae! Ullam excepturi eos debitis odit iure nemo
+        laborum expedita vero nostrum voluptates officia accusantium tempore
+        nisi, quidem quibusdam repudiandae qui culpa. Perferendis odio sit
+        exercitationem cupiditate. Consectetur autem debitis eum assumenda rem
+        repudiandae, nemo voluptate. Numquam molestiae, eum molestias voluptatum
+        quisquam facilis odit tempora veritatis.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempore
+        ratione dolorum deserunt fugit magnam, quia autem. Itaque et, asperiores
+        eveniet exercitationem totam ad velit molestias, consectetur est id
+        magni!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
