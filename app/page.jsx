4182a573809e91de4b912732961eaf9c2b06d8bc
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-24 11:47:48 +0200

    update home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,31 @@
-import Link from "next/link";
 import React from "react";
+import "./globals.css";
+import myImg from "../public/openYourEyesMorty.png";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to Pages directory next.js</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
+      <h1 style={{ textAlign: "center" }}>
+        Welcome to Pages directory next.js
+      </h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Image src={myImg} alt="Open Your Eyes Morty" />
+      </div>
+      <div className="home-article">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+          veniam earum ipsa voluptates amet accusantium debitis magnam saepe
+          consequatur dolor maxime hic accusamus quia, deleniti quasi optio
+          doloribus nulla recusandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quo
+          eveniet voluptatibus voluptates incidunt aliquid, odio dolore alias
+          accusantium repudiandae aperiam non deserunt eius, laudantium
+          voluptatem provident assumenda porro eligendi.
+        </p>
+      </div>
     </div>
   );
 };
