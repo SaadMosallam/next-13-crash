448d884dd7d716364a0436d0733a510f6c790446
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-24 17:52:19 +0200

    add courses api

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
+import myImg from "../../public/openYourEyesMorty.png";
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="container">
-        <div className="logo">
-          <Link href="/">App Directory Next.js</Link>
-        </div>
-        <div className="links">
-          <Link href="/about">About</Link>
-          <Link href="/about/team">Our Team</Link>
-          <Link href="/code/repos">Code</Link>
-        </div>
+      <div className="logo">
+        <Link href="/">
+          <Image width={100} src={myImg} alt="Open Your Eyes Morty" />
+          Open Your Eyes Morty
+        </Link>
+      </div>
+
+      <div className="links">
+        <Link href="/about">About</Link>
+        <Link href="/about/team">Our Team</Link>
+        <Link href="/code/repos">Code</Link>
       </div>
     </header>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import myImg from "../public/openYourEyesMorty.png";
-import Image from "next/image";
+import Courses from "./components/Courses";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>
-        Welcome to Pages directory next.js
-      </h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Image src={myImg} alt="Open Your Eyes Morty" />
-      </div>
-      <div className="home-article">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-          veniam earum ipsa voluptates amet accusantium debitis magnam saepe
-          consequatur dolor maxime hic accusamus quia, deleniti quasi optio
-          doloribus nulla recusandae.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quo
-          eveniet voluptatibus voluptates incidunt aliquid, odio dolore alias
-          accusantium repudiandae aperiam non deserunt eius, laudantium
-          voluptatem provident assumenda porro eligendi.
-        </p>
-      </div>
+      <h1 style={{ textAlign: "center" }}>Using Pages directory next.js</h1>
+      <Courses />
     </div>
   );
 };
