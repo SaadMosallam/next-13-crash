4bfc058a59d9c9d8a43970f6fc27df9881decca7
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-24 14:56:38 +0200

    add page metadata

--- app/about/team/page.jsx
+++ app/about/team/page.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+  title: "Team page",
+  description: "A page dedicated for our amazing team",
+};
+
 const TeamPage = () => {
   return (
     <div>
