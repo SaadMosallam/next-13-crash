8d3bc270d66e8fd6fc293a51e5155014a292e4eb
Saad Mosallam <saad.mosallam@4sale.tech>
2023-12-24 11:47:59 +0200

    add about layout

---
+++ app/about/layout.jsx
@@ -0,0 +1,28 @@
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import Link from "next/link";
+
+export const metadata = {
+  title: "About Page",
+  description: "About Page for data about our website",
+  keywords:
+    "web development, web design, javascript, react, next, node, html, css",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <h1
+        className="links header"
+        style={{
+          margin: 0,
+          border: "5px solid",
+          borderTop: "none",
+        }}
+      >
+        <Link href="/about">That's a special inner Layout for About dir</Link>
+      </h1>
+      <main className="container">{children}</main>
+    </>
+  );
+}
